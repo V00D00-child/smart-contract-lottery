6f65efc10fcfae9d9448080f7a4e44612d3c04b8
Idris Bowman <corndog2@Idriss-MacBook-Air.local>
2021-10-17 15:19:43 -0400

    add smart contract test

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # smart-contract-lotter
-Lottery smart contracts
+Lottery smart contracts. Player must submit a minimum ammount of 0.02 ETH to enter into the lottery pool. After all players have entered, the manger address(account that deployed contract) can call the pickWinner() function. The winning will be picked using a pseudo random generator and the winning address with receive all ETH in the lottery pool. After the lottery winner is selected the smart contacts state will reset and a new lottery round can start.
 
 # Contract deployed
-- ropsten: 
+- ropsten:  
 - https://ropsten.etherscan.io/address/
+
+# Test Contract locally
+- npm run test
+
+# Deploy Contract
+1. Create .env file locally
+2. Using the .ene.example fill in the required feilds
+3. Use deploy script npm run deploy
+
+Example of what a evn may look like
+PRIVATE_KEYS="DRVE4335G55656555JJJFDDS3"
+INFURA_API_KEY=JJJSFB55554BJG4
+NETWORK=kovan
+
+
--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -3,20 +3,120 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require('../compile');
+const EVM_REVERT = 'VM Exception while processing transaction: revert';
 
-let accounts;
-let lottery;
+let lotteryContract;
+let deployerAccount;
+let playerOneAccount;
+let playerTwoAccount;
+let playerThreeAccount;
 
 beforeEach(async () => {
-    accounts = await web3.eth.getAccounts();
+    // Load accounts
+    const accounts = await web3.eth.getAccounts();
+    deployerAccount = accounts[0];
+    playerOneAccount = accounts[1];
+    playerTwoAccount = accounts[2];
+    playerThreeAccount = accounts[3];
 
-    lottery = await new web3.eth.Contract(JSON.parse(interface))
+    lotteryContract = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode })
-        .send({ from: accounts[0], gas: '1000000' });
+        .send({ from: deployerAccount, gas: '1000000' });
 });
 
 describe('Lottery smart contract', () => {
     it('Deploys a contract', () => {
-        assert.ok(lottery.options.address);
+        assert.ok(lotteryContract.options.address);
+    });
+
+    it('Should set the manager of the contact as the account that deployed the contract', async () => {
+        const manger = await lotteryContract.methods.manager().call();
+        assert.equal(manger, deployerAccount);
+    });
+
+    it('Should allow one account to enter to into lottery', async () => {
+        await lotteryContract.methods.enter().send({
+            from: playerOneAccount,
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lotteryContract.methods.getPlayers().call({
+            from: deployerAccount
+        });
+
+        assert.equal(playerOneAccount, players[0]);
+        assert.equal(1, players.length);
+    });
+
+    it('Should allow multiple accounts to enter into lottery', async () => {
+        await lotteryContract.methods.enter().send({
+            from: playerOneAccount,
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        await lotteryContract.methods.enter().send({
+            from: playerTwoAccount,
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        await lotteryContract.methods.enter().send({
+            from: playerThreeAccount,
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lotteryContract.methods.getPlayers().call({
+            from: deployerAccount
+        });
+
+        assert.equal(playerOneAccount, players[0]);
+        assert.equal(playerTwoAccount, players[1]);
+        assert.equal(playerThreeAccount, players[2]);
+        assert.equal(3, players.length);
+    });
+
+    it('Should require a minimum amount of ether to enter', async () => {
+        try {
+            await lotteryContract.methods.enter().send({
+                from: playerOneAccount,
+                value: 0
+            });
+        } catch (err) {
+            assert(err);
+            assert.equal(EVM_REVERT, err.message);
+        }
+    });
+
+    it('Should restrict calling pickWinner() to only manager account', async () => {
+        try {
+            await lotteryContract.methods.pickWinner().send({
+                from: playerOneAccount
+            });
+        } catch (err) {
+            assert(err);
+            assert.equal(EVM_REVERT, err.message);
+        }
+    });
+
+    it('Should send money to winner account and rest the players array', async () => {
+        await lotteryContract.methods.enter().send({
+            from: playerOneAccount,
+            value: web3.utils.toWei('2', 'ether')
+        });
+
+        const initalBalance = await web3.eth.getBalance(playerOneAccount);
+        await lotteryContract.methods.pickWinner().send({ from: deployerAccount });
+        const finalBalance = await web3.eth.getBalance(playerOneAccount);
+        const difference = finalBalance - initalBalance;
+
+        assert(difference > web3.utils.toWei('1.8', 'ether'));
+
+        // Reset contact state 
+        const players = await lotteryContract.methods.getPlayers().call({
+            from: deployerAccount
+        });
+        assert.equal(0, players.length);
+
+        const loteryContractBalance = await web3.eth.getBalance(lotteryContract.options.address);
+        assert.equal(0, loteryContractBalance);
     });
 });
