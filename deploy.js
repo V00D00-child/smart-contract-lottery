5a06ebe5a2865fae7db4b8f6cbc685249a8b2274
Idris Bowman <corndog2@Idriss-MacBook-Air.local>
2021-10-21 15:05:31 -0400

    add v2 contract

--- deploy.js
+++ deploy.js
@@ -18,11 +18,15 @@ const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
 
     console.log('Attempting to deploy from account', accounts[0]);
+    console.log('_____________________________________________________________________')
 
     const result = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode })
         .send({ from: accounts[0], gas: '1000000', gasPrice: '5000000000' });
 
+    console.log('Creation result:', result);
+    console.log('_____________________________________________________________________')
     console.log(`Contract deployed to ${NETWORK} at address: ${result.options.address}`);
+    console.log('_____________________________________________________________________')
 };
 deploy();
